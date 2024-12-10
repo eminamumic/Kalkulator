2c1882776f4f378a62b824f1d8fb15ab9f07384c
eminamumic <emimumic1@gmail.com>
2024-12-10 21:29:23 +0100

    Funkcija korijena dodana

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-
-function mnozenje(broj1,broj2){
-    let rezultat;
-    rezultat=broj1*broj2;
-    console.log(`Rezultat mnozenja je ${rezultat}`);
-    
+function mnozenje(broj1, broj2) {
+  let rezultat
+  rezultat = broj1 * broj2
+  console.log(`Rezultat mnozenja brojeva ${broj1} i ${broj2} je${rezultat}`)
 }
-mnozenje(3,2);
+mnozenje(3, 2)
 
-function sabiranje (num1, num2){
-    let sum;
-    sum = Number(num1) + Number(num2);
-    console.log(`Zbir je ${sum}`)
+function sabiranje(num1, num2) {
+  let sum
+  sum = Number(num1) + Number(num2)
+  console.log(`Rezultat zbira brojeva ${num1} i ${num2} je ${sum}`)
 }
 
 sabiranje(8, 3)
 
 function kvadrat(baza, stepen) {
-  console.log("Kvadrat je ${baza ** stepen}")
+  let kvadrat = 0
+  kvadrat = Number(baza) ** Number(stepen)
+  console.log(
+    `Rezultat kvadrata broja ${baza} na ${stepen} stepen je ${kvadrat}`
+  )
 }
 
+kvadrat(4, 2)
 
+function korijenBroja(broj) {
+  let rezultat
+  rezultat = Math.sqrt(broj)
+  console.log(`Kvadratni korijen broja ${broj} je ${rezultat}`)
+}
+
+korijenBroja(25)
